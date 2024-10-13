377eb8d030789923c568c81135473ca1c186cd64
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2024-10-13 23:47:36 +0200

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,60 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "shecancodeschool.org",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+    domains: [
+      "lh3.googleusercontent.com",
+      "firebasestorage.googleapis.com",
+      "res.cloudinary.com",
+      "avatars.githubusercontent.com",
+      "i.ibb.co",
+      "images.unsplash.com",
+      "shecancodeschool.org",
+    ],
+  },
+};
+
+export default nextConfig;
